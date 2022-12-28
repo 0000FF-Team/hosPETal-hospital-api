fb752112e8612dfce43b30505fc7b4d571415d1c
River Kim <bluuminn@gmail.com>
2022-12-28 14:02:55 +0900

    Initial commit

---
+++ src/main/java/com/hospetal/hospital/HosPeTalHospitalApiApplication.java
@@ -0,0 +1,13 @@
+package com.hospetal.hospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HosPeTalHospitalApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HosPeTalHospitalApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hospetal/hospital/HosPeTalHospitalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospetal.hospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HosPeTalHospitalApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
